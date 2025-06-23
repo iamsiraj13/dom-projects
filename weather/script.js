ee531991d21e83ff6985c838ba6ea2807357f3d4
iamsiraj13 <iamsiraj13@gmail.com>
2025-06-24 02:33:58 +0600

    complete

--- weather/script.js
+++ weather/script.js
@@ -2,6 +2,7 @@ const api = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const api_key = "20b5585b37889f2ef7815be8bece94e4";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city = "Dhaka") {
   const res = await fetch(api + city + `&appid=${api_key}`);
   const data = await res.json();
@@ -10,6 +11,18 @@ async function checkWeather(city = "Dhaka") {
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
 }
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
